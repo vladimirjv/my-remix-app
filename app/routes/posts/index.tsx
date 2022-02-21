f113399e3e97b16229632649b35798898b3e645b
VladimirJV <ivladimirjuarez@outlook.com>
2022-02-21 13:33:52 -0600

    init

---
+++ app/routes/posts/index.tsx
@@ -0,0 +1,25 @@
+import { Link, useLoaderData } from "remix";
+import type {Post} from "~/post";
+import { getPosts } from "~/post";
+
+export const loader = async () => {
+	return getPosts();
+};
+
+export default function Posts() {
+  const posts = useLoaderData<Post[]>();
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline text-blue-600">Posts</h1>
+
+			<ul>
+				{posts.map(post => (
+					<li key={post.slug}>
+						<Link to={post.slug}>{post.title}</Link>
+					</li>
+				))}
+			</ul>
+    </div>
+  );
+}
